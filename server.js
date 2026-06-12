270dfafd60aa7b68fe6ce5b73f6c24d43f971f20
xianrentesuchi-bit <xianrentesuchi@gmail.com>
2026-06-13 07:39:26 +0900

    Update server.js

--- server.js
+++ server.js
@@ -4,127 +4,173 @@ const http = require("http").createServer(app);
 const io = require("socket.io")(http);
 const path = require("path");
 
-// 静的ファイルの提供（index.htmlを配置するディレクトリ）
 app.use(express.static(__dirname));
 
 app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "index.html"));
 });
 
-// オンライン上の全ゲームルーム・全プレイヤーを管理するオブジェクト
-let onlinePlayers = {};
-let activeBullets = {};
-let activeCubes = {};
-let activeSandbags = {};
-let globalGameState = "SELECT";
-let globalGameMode = "DUEL";
+// ルームごとのゲーム状態を保持するオブジェクト
+// rooms[roomCode] = { players: {}, gameState: "SELECT", gameMode: "DUEL" }
+let rooms = {};
 
 io.on("connection", (socket) => {
   console.log(`プレイヤーが接続しました: ${socket.id}`);
 
-  // 新しいプレイヤーが入室した初期状態
-  onlinePlayers[socket.id] = {
-    id: socket.id,
-    x: 0,
-    y: 0,
-    type: null,
-    hp: 0,
-    maxHp: 0,
-    radius: 20,
-    speed: 0,
-    ammo: 3,
-    reloadTimer: 0,
-    shootCooldown: 0,
-    ultCharge: 0,
-    ultActive: false,
-    isJumping: false,
-    jumpProgress: 0,
-    healTimer: 0,
-    kbVx: 0,
-    kbVy: 0,
-    isDashing: false,
-    dashAngle: 0,
-    dashDistanceTraveled: 0,
-    dashMaxDistance: 0,
-    cubes: 0,
-    longDashTimer: 0,
-    facingAngle: 0,
-    isBot: false,
-    name: "Player"
-  };
+  // 1. ルームへの入室処理
+  socket.on("joinRoom", (roomCode) => {
+    // アルファベットは大文字に統一して、前後の空白を削除
+    const code = roomCode.toUpperCase().trim();
+    
+    // 該当ルームがなければ新しく作成
+    if (!rooms[code]) {
+      rooms[code] = {
+        players: {},
+        globalGameState: "SELECT",
+        globalGameMode: "DUEL"
+      };
+    }
 
-  // 現在のゲーム状態を新規接続者に同期
-  socket.emit("initGameState", {
-    players: onlinePlayers,
-    gameMode: globalGameMode,
-    gameState: globalGameState
+    // Socket.ioのルーム機能を使ってクライアントを隔離・所属させる
+    socket.join(code);
+    // ソケット自体にルームコードを記憶させておく（切断時などのため）
+    socket.roomCode = code;
+
+    // 新しいプレイヤーの初期データをその部屋用に作成
+    rooms[code].players[socket.id] = {
+      id: socket.id,
+      x: 0,
+      y: 0,
+      type: null,
+      hp: 0,
+      maxHp: 0,
+      radius: 20,
+      speed: 0,
+      ammo: 3,
+      reloadTimer: 0,
+      shootCooldown: 0,
+      ultCharge: 0,
+      ultActive: false,
+      isJumping: false,
+      jumpProgress: 0,
+      healTimer: 0,
+      kbVx: 0,
+      kbVy: 0,
+      isDashing: false,
+      dashAngle: 0,
+      dashDistanceTraveled: 0,
+      dashMaxDistance: 0,
+      cubes: 0,
+      longDashTimer: 0,
+      facingAngle: 0,
+      isBot: false,
+      name: "Player"
+    };
+
+    // 入室したプレイヤーに現在の「その部屋の」ゲーム状態を同期
+    socket.emit("initGameState", {
+      players: rooms[code].players,
+      gameMode: rooms[code].globalGameMode,
+      gameState: rooms[code].globalGameState
+    });
+
+    console.log(`ユーザー ${socket.id} がルーム [${code}] に入室しました`);
   });
 
-  // モード切り替えの同期
+  // モード切り替えの同期（同じ部屋のメンバーにのみ通知）
   socket.on("changeMode", (mode) => {
-    globalGameMode = mode;
-    socket.broadcast.emit("modeChanged", mode);
+    const code = socket.roomCode;
+    if (code && rooms[code]) {
+      rooms[code].globalGameMode = mode;
+      socket.to(code).emit("modeChanged", mode);
+    }
   });
 
-  // プレイヤーがキャラクターを選択して出撃したとき
+  // キャラクターを選択して出撃したとき
   socket.on("joinGame", (data) => {
-    if (onlinePlayers[socket.id]) {
-      onlinePlayers[socket.id].type = data.type;
-      onlinePlayers[socket.id].x = data.x;
-      onlinePlayers[socket.id].y = data.y;
-      onlinePlayers[socket.id].hp = data.hp;
-      onlinePlayers[socket.id].maxHp = data.maxHp;
-      onlinePlayers[socket.id].radius = data.radius;
-      onlinePlayers[socket.id].speed = data.speed;
-      onlinePlayers[socket.id].name = data.type.name;
+    const code = socket.roomCode;
+    if (code && rooms[code] && rooms[code].players[socket.id]) {
+      rooms[code].players[socket.id].type = data.type;
+      rooms[code].players[socket.id].x = data.x;
+      rooms[code].players[socket.id].y = data.y;
+      rooms[code].players[socket.id].hp = data.hp;
+      rooms[code].players[socket.id].maxHp = data.maxHp;
+      rooms[code].players[socket.id].radius = data.radius;
+      rooms[code].players[socket.id].speed = data.speed;
+      rooms[code].players[socket.id].name = data.type.name;
       
-      globalGameState = "PLAYING";
+      rooms[code].globalGameState = "PLAYING";
       
-      // 全員に通知
-      io.emit("playerJoined", onlinePlayers[socket.id]);
+      // ルーム内の全員に通知
+      io.to(code).emit("playerJoined", rooms[code].players[socket.id]);
     }
   });
 
   // リアルタイムの位置・ステータス更新の同期
   socket.on("updateStatus", (statusData) => {
-    if (onlinePlayers[socket.id]) {
-      onlinePlayers[socket.id] = { ...onlinePlayers[socket.id], ...statusData };
-      // 自分以外の全員に位置変更を送信
-      socket.broadcast.emit("remotePlayerUpdated", onlinePlayers[socket.id]);
+    const code = socket.roomCode;
+    if (code && rooms[code] && rooms[code].players[socket.id]) {
+      rooms[code].players[socket.id] = { ...rooms[code].players[socket.id], ...statusData };
+      // 自分以外のルーム内メンバーに位置変更を送信
+      socket.to(code).emit("remotePlayerUpdated", rooms[code].players[socket.id]);
     }
   });
 
   // 弾が発射されたときのアクション同期
   socket.on("fireBullet", (bulletData) => {
-    io.emit("remoteBulletFired", bulletData);
+    const code = socket.roomCode;
+    if (code) {
+      io.to(code).emit("remoteBulletFired", bulletData);
+    }
   });
 
   // キューブ生成・同期
   socket.on("spawnCube", (cubeData) => {
-    io.emit("remoteCubeSpawned", cubeData);
+    const code = socket.roomCode;
+    if (code) {
+      io.to(code).emit("remoteCubeSpawned", cubeData);
+    }
   });
 
   // サンドバッグ同期
   socket.on("updateSandbag", (bagData) => {
-    io.emit("remoteSandbagUpdated", bagData);
+    const code = socket.roomCode;
+    if (code) {
+      io.to(code).emit("remoteSandbagUpdated", bagData);
+    }
   });
 
   // 壁破壊の同期
   socket.on("destroyWall", (wallIdx) => {
-    io.emit("remoteWallDestroyed", wallIdx);
+    const code = socket.roomCode;
+    if (code) {
+      io.to(code).emit("remoteWallDestroyed", wallIdx);
+    }
   });
 
   // ゲームオーバー状態の同期
   socket.on("triggerGameOver", (data) => {
-    globalGameState = "GAMEOVER";
-    io.emit("remoteGameOver", data);
+    const code = socket.roomCode;
+    if (code && rooms[code]) {
+      rooms[code].globalGameState = "GAMEOVER";
+      io.to(code).emit("remoteGameOver", data);
+    }
   });
 
   // 切断時の処理
   socket.on("disconnect", () => {
+    const code = socket.roomCode;
     console.log(`プレイヤーが切断しました: ${socket.id}`);
-    delete onlinePlayers[socket.id];
-    io.emit("playerLeft", socket.id);
+    if (code && rooms[code]) {
+      delete rooms[code].players[socket.id];
+      io.to(code).emit("playerLeft", socket.id);
+
+      // 部屋に誰もいなくなったらメモリ解放のためにルームを削除
+      if (Object.keys(rooms[code].players).length === 0) {
+        delete rooms[code];
+        console.log(`ルーム [${code}] の空室に伴う削除を行いました`);
+      }
+    }
   });
 });
 
